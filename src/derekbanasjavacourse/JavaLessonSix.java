04813c90a9644b65fd1f08c9004f8ce6deb09e78
Samuel Nadim <wnadim2992@gmail.com>
2026-01-27 19:38:29 -0500

    fix structure

--- src/derekbanasjavacourse/JavaLessonSix.java
+++ src/derekbanasjavacourse/JavaLessonSix.java
@@ -3,16 +3,20 @@ package derekbanasjavacourse;
 import java.io.*;
 
 public class JavaLessonSix {
-	
-	public static void main(String[] args) // throws IOException, FileNotFoundException
-	{
+
+	public static void main(String[] args) {
 		getAFile("./someStuff.txt");
 	}
-	
 
-	public static void getAFile(String fileName)// throws IOException, FileNotFoundException
-	{
-		FileInputStream file = new FileInputStream(fileName);	
+
+	public static void getAFile(String fileName) {
+		try {
+			FileInputStream file = new FileInputStream(fileName);
+			System.out.println("File found successfully!");
+		} catch (FileNotFoundException e) {
+			System.out.println("Error: The file was not found.");
+			e.printStackTrace();
+		}
 	}
-		
+
 }
