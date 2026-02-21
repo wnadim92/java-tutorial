17d8882ee99bc500b3d971849e1a67a416a6adf3
Samuel Nadim <wnadim2992@gmail.com>
2026-02-21 13:46:46 -0500

    GetTime20

---
+++ src/derekbanasjavacourse/LessonSeventeen.java
@@ -0,0 +1,21 @@
+package derekbanasjavacourse;
+
+public class LessonSeventeen {
+
+
+    public static void main(String[] args) {
+
+        Thread getTime = new GetTime20();
+
+        Runnable getMail = new GetTheMail(10);
+
+        Runnable getMailAgain = new GetTheMail(20);
+
+        getTime.start();
+        new Thread(getMail).start();
+        new Thread(getMailAgain).start();
+
+
+    }
+
+}
